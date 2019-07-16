3d8c3295a34ab5507c404e9d33b3491faeb5d608
gamine <48608531+gamin-yojo@users.noreply.github.com>
2019-07-16 11:40:19 +0900

    "ディレクトリ作成"

--- make_html.sh
+++ make_html.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-#HTML,CSS,JavaScriptのテンプレをコピペするだけのもの。
+#CSS,JavaScriptのテンプレをコピペするだけのもの。
 #テンプレはhtml-temp参照
 
+mkdir css
+mkdir js
+mkdir img
 cp -r -p ~/template/html-temp/* .
 #html-tempを置いた場所にパスを設定しておく
 echo "HTML, CSS and JS were created."
